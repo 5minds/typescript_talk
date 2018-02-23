275f512872cb664e923f07ddb3e6b57a91d61a8f
5ebastianMeier <sebastian.meier@5minds.de>
2018-02-23 10:51:17 +0100

    add first examples

---
+++ examples/01_javascript_migrated_to_typescript/index.ts
@@ -0,0 +1,17 @@
+
+export class MyBasicClass {
+  
+  public doSomething(anything: any): void {
+
+    if (!anything) {
+      throw new Error('give me something');
+    }
+
+    if (typeof anything !== 'string') {
+      throw new Error('give me something else');
+    }
+
+    console.log(`did something: ${anything}`);
+  }
+
+}
