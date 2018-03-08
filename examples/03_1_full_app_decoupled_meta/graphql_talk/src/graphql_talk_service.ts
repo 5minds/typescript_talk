9c4bb6ab9e59b74fff4d4591b40b73f99c9f1701
5ebastianMeier <sebastian.meier@5minds.de>
2018-03-08 16:35:42 +0100

    add postman collection

--- examples/03_1_full_app_decoupled_meta/graphql_talk/src/graphql_talk_service.ts
+++ examples/03_1_full_app_decoupled_meta/graphql_talk/src/graphql_talk_service.ts
@@ -9,13 +9,13 @@ export class GraphQlTalkService implements ITalkService {
     const response: AudienceResponse = audience.tell('GraphQl is cool too');
 
     switch (response) {
-      case AudienceResponse.Great: logger.info('told ya\n');
+      case AudienceResponse.Great: logger.info('told ya 😎\n');
         break;
-      case AudienceResponse.Medium: logger.info(`it's more fun if you use it yourself\n`);
+      case AudienceResponse.Medium: logger.info(`it's more fun if you use it yourself 🤠\n`);
         break;
-      case AudienceResponse.Bad: logger.info('whatever dude\n');
+      case AudienceResponse.Bad: logger.info('whatever 😡\n');
         break;
-      default: logger.error('where is the audience o_O\n');
+      default: logger.error('where is the audience o_O 😨\n');
         break;
     }
   }
--- roter_faden_seb.md
+++
@@ -1,8 +0,0 @@
-Shared Code             2 min
-Interfaces              2 min
-Pakete                  2 min
-Contracts               2 min
-Dependency Injection    2 min
-Demo vorführen          8 min
------------------------------
-                       18 min
